68e4cba55c48eaa01f17af54a37dca3fea549f7d
Fabio Bastian <irai852@gmail.com>
2019-03-30 13:51:38 +0000

    changes to TCP filtering and created wirespy

--- alltraffic.go
+++ alltraffic.go
@@ -7,104 +7,93 @@
 package filters
 
 import (
+	"net"
+
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	log "github.com/sirupsen/logrus"
+
 	// "github.com/irai/base"
-	"github.com/irai/netfilter/config"
-	"github.com/irai/netfilter/model"
 	"sync"
 	"time"
+
+	"github.com/irai/netfilter/network"
 )
+
+type HostTraffic struct {
+	MAC     net.HardwareAddr `json:"mac" bson:"mac"`
+	Blocked bool             `json:"client_blocked" bson:"client_blocked"`
+	Traffic []*TCPTraffic
+}
+
+func (h *HostTraffic) findOrAddIP(ip net.IP) (entry *TCPTraffic) {
+	defer mutex.Unlock()
+
+	mutex.Lock()
+	for _, entry = range h.Traffic {
+		if entry.IP.Equal(ip) {
+			return entry
+		}
+	}
+	entry = &TCPTraffic{IP: network.DupIP(ip)}
+	h.Traffic = append(h.Traffic, entry)
+	return entry
+}
+
+type TCPTraffic struct {
+	IP             net.IP    `json:"client_ip" bson:"client_ip"`
+	LastPacketTime time.Time `json:"last_packet_time" bson:"last_packet_time"`
+	OutPacketBytes uint      `json:"out_bytes" bson:"out_bytes"`
+	OutPacketCount uint      `json:"out_packet_count" bson:"out_packet_count"`
+	InPacketBytes  uint      `json:"in_bytes" bson:"in_bytes"`
+	InPacketCount  uint      `json:"in_packet_count" bson:"in_packet_count"`
+	OutConnCount   uint      `json:"out_conn_count" bson:"out_conn_count"`
+}
 
 var (
-	deviceId string
-
-	trafficTable []model.IPTraffic = make([]model.IPTraffic, 0, 1024)
-	blockTable   []model.IPTraffic
-	tableMutex   sync.Mutex
+	table map[string]*HostTraffic = map[string]*HostTraffic{}
+	mutex sync.Mutex
 )
 
-/***
-func PostIPTraffic(hostname string, deviceId string, IPTable []model.IPTraffic) (err error) {
+func FindMAC(mac net.HardwareAddr) *HostTraffic {
+	defer mutex.Unlock()
 
-	aurl := fmt.Sprintf("http://%s:8080/traffic/v1/device/%s/traffic", hostname, deviceId)
-	fields := url.Values{}
-
-	fields.Set("device_id", deviceId)
-	for i, _ := range IPTable {
-		fields.Add("client_mac", IPTable[i].ClientMAC)
-		fields.Add("client_ip", IPTable[i].ClientIP)
-		fields.Add("client_port", strconv.FormatUint(uint64(IPTable[i].ClientPort), 10))
-		fields.Add("server_ip", IPTable[i].ServerIP)
-		fields.Add("server_port", strconv.FormatUint(uint64(IPTable[i].ServerPort), 10))
-		fields.Add("start_time", IPTable[i].StartTime.String())
-		fields.Add("end_time", IPTable[i].LastTime.String())
-		fields.Add("out_bytes", strconv.FormatUint(uint64(IPTable[i].OutPacketBytes), 10))
-		fields.Add("out_packet_count", strconv.FormatUint(uint64(IPTable[i].OutPacketCount), 10))
-		fields.Add("in_bytes", strconv.FormatUint(uint64(IPTable[i].InPacketBytes), 10))
-		fields.Add("in_packet_count", strconv.FormatUint(uint64(IPTable[i].InPacketCount), 10))
-		fields.Add("out_conn_count", strconv.FormatUint(uint64(IPTable[i].OutConnCount), 10))
-	}
-
-	resp, err := post(nil, aurl, fields)
-	if err != nil {
-		log.Error("TrafficProxy error posting ", aurl, err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	return nil
-
+	mutex.Lock()
+	return table[mac.String()]
 }
-***/
 
-// Should use http://info.io to lookup names and geo
-func PrintTable(t []model.IPTraffic) {
-	if len(t) <= 0 {
+func findOrAddMAC(mac net.HardwareAddr) (entry *HostTraffic) {
+	defer mutex.Unlock()
+
+	mutex.Lock()
+	entry, ok := table[mac.String()]
+	if !ok {
+		entry = &HostTraffic{MAC: network.DupMAC(mac), Traffic: []*TCPTraffic{}}
+		table[mac.String()] = entry
+	}
+	return entry
+}
+
+// PrintTable prints the table to standard out
+// TODO: Should use http://info.io to lookup names and geo
+func PrintTable() {
+	if len(table) <= 0 {
 		return
 	}
 
-	log.Printf("Traffic Table len %d", len(t))
-	log.Println("MAC                server         port  ccount  inpkt     inbytes outpkt   outbytes")
+	log.Printf("Traffic Table len %d", len(table))
+	log.Println("MAC                 IP         outconn  inpkt     inbytes outpkt   outbytes")
 
-	for i, _ := range t {
-		log.Printf("%16s %15s %4d %5d %6d %10d %6d %10d", t[i].ClientMAC, t[i].ServerIP, t[i].ServerPort,
-			t[i].OutConnCount, t[i].InPacketCount, t[i].InPacketBytes, t[i].OutPacketCount, t[i].OutPacketBytes)
+	for _, host := range table {
+		for _, t := range host.Traffic {
+			log.Printf("%16s %15s %7d %6d %10d %6d %10d", host.MAC.String(), t.IP,
+				t.OutConnCount, t.InPacketCount, t.InPacketBytes, t.OutPacketCount, t.OutPacketBytes)
+		}
 	}
 }
 
-func AllTraffic(config *config.Config) {
-	const snapshot_len int32 = 1024
-	const promiscuous bool = true
-	const timeout time.Duration = 10 * time.Second
-	// handle  *pcap.Handle
-
-	handle, err := pcap.OpenLive(config.NIC, snapshot_len, promiscuous, timeout)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer handle.Close()
-
-	log.Info("Started AllTraffic() goroutine")
-	// Set filter
-	// var filter string = "tcp and (port 80 or port 443)"
-	// var filter string = "tcp and port 80"
-	var filter string = "tcp"
-	err = handle.SetBPFFilter(filter)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Use the handle as a packet source to process all packets
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSource.Packets() {
-		//filters.PrintPacketInfo(packet)
-		captureTcpTraffic(config, packet)
-	}
-}
-
+/****
 func TrafficProxyLoop(gwHostName string, deviceId string) {
 	ticker := time.NewTicker(time.Second * 60)
 	for range ticker.C {
@@ -128,30 +117,39 @@ func TrafficProxyLoop(gwHostName string, deviceId string) {
 		}
 	}
 }
+***/
 
-func findMACEntry(entry model.IPTraffic) (ret *model.IPTraffic) {
-	// get a copy of the slice in case it concurrently reset by the sending goroutine
-	tableMutex.Lock()
-	t := trafficTable
-	tableMutex.Unlock()
+func ListenAndServe(nic string) {
+	const snapshot_len int32 = 1024
+	const promiscuous bool = true
+	const timeout time.Duration = 10 * time.Second
+	// handle  *pcap.Handle
 
-	for i, _ := range t {
-		if t[i].ClientMAC == entry.ClientMAC &&
-			t[i].ServerIP == entry.ServerIP && t[i].ServerPort == entry.ServerPort {
-			return &t[i]
-		}
+	handle, err := pcap.OpenLive(nic, snapshot_len, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	log.Info("Started AllTraffic() goroutine")
+	// Set filter
+	// var filter string = "tcp and (port 80 or port 443)"
+	// var filter string = "tcp and port 80"
+	var filter string = "tcp"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	tableMutex.Lock()
-	trafficTable = append(trafficTable, entry)
-	ret = &trafficTable[len(trafficTable)-1]
-	tableMutex.Unlock()
-
-	// log.WithFields(log.Fields{"src": base.FunctionName(2), "MAC": entry.ClientMAC, "Server": entry.ServerIP, "Port": entry.ServerPort}).Debug("filter added")
-	return ret
+	// Use the handle as a packet source to process all packets
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		// PrintPacketInfo(packet)
+		captureTcpTraffic(packet)
+	}
 }
 
-func captureTcpTraffic(config *config.Config, packet gopacket.Packet) {
+func captureTcpTraffic(packet gopacket.Packet) {
 	ethLayer := packet.Layer(layers.LayerTypeEthernet)
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	tcpLayer := packet.Layer(layers.LayerTypeTCP)
@@ -168,38 +166,29 @@ func captureTcpTraffic(config *config.Config, packet gopacket.Packet) {
 		tcp, _ := tcpLayer.(*layers.TCP)
 
 		now := time.Now()
-		entry := model.IPTraffic{ClientMAC: eth.SrcMAC.String(), StartTime: now, LastTime: now}
+
+		// Don't capture netfilter traffic
+		// if ip.SrcIP.Equal(config.HostIP) || ip.DstIP.Equal(config.HostIP) {
+		// return
+		// }
+
+		host := findOrAddMAC(eth.SrcMAC)
+		entry := host.findOrAddIP(ip.SrcIP)
 
 		tcpLen := uint(ip.Length - uint16(ip.IHL*4))
 
-		// Don't capture netfilter traffic
-		if ip.SrcIP.Equal(config.HostIP) || ip.DstIP.Equal(config.HostIP) {
-			return
+		// entry.IP = ip.SrcIP.String()
+		entry.LastPacketTime = now
+		entry.OutPacketBytes = entry.OutPacketBytes + tcpLen
+		entry.OutPacketCount = entry.OutPacketCount + 1
+		if tcp.SYN {
+			entry.OutConnCount = entry.OutConnCount + 1
 		}
 
-		// Clients are always on the LAN
-		if config.NetfilterLAN.Contains(ip.SrcIP) {
-			entry.ClientIP = ip.SrcIP.String()
-			entry.ClientPort = uint16(tcp.SrcPort)
-			entry.ServerIP = ip.DstIP.String()
-			entry.ServerPort = uint16(tcp.DstPort)
-			e := findMACEntry(entry)
-			// e.LastTime = time.Now()
-			e.OutPacketBytes = e.OutPacketBytes + tcpLen
-			e.OutPacketCount = e.OutPacketCount + 1
-			if tcp.SYN {
-				e.OutConnCount = e.OutConnCount + 1
-			}
-		} else if config.NetfilterLAN.Contains(ip.DstIP) {
-			entry.ClientMAC = eth.DstMAC.String()
-			entry.ClientIP = ip.DstIP.String()
-			entry.ClientPort = uint16(tcp.DstPort)
-			entry.ServerIP = ip.SrcIP.String()
-			entry.ServerPort = uint16(tcp.SrcPort)
-			e := findMACEntry(entry)
-			// e.LastTime = time.Now()
-			e.InPacketBytes = e.InPacketBytes + tcpLen
-			e.InPacketCount = e.InPacketCount + 1
-		}
+		host = findOrAddMAC(eth.DstMAC)
+		entry = host.findOrAddIP(ip.DstIP)
+		entry.LastPacketTime = now
+		entry.InPacketBytes = entry.InPacketBytes + tcpLen
+		entry.InPacketCount = entry.InPacketCount + 1
 	}
 }
